3d39029a3a91111623bcbdcdee450e4bdf3f7a39
Narek <mnk80898@gmail.com>
2020-03-20 18:19:10 +0300

    default commit messege

--- hello.java
+++ hello.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 class HelloWorld {
 	public static void main( String []args ) {
 		Scanner scan = new Scanner(System.in);
+
 		System.out.println("What is your name?");
 		String name = scan.nextLine();
-		System.out.printf("Hello %s\n", name);
+		System.out.printf("Hello, %s!\n", name);
 	}
 }
